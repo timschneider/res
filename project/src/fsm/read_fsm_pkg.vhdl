8dca9c63d6cdd63917203323d64bd8ed93697025
mox <de_mox@zoho.com>
2016-07-17 16:49:12 +0200

    Forgot to add the status enum for the read fsm.

---
+++ project/src/fsm/read_fsm_pkg.vhdl
@@ -0,0 +1,7 @@
+package read_fsm_pkg is
+	type read_fsm_state_type is ( idl_rdt, cmp_dlv, req0, req1, rd0, rd1_keep, rd1, rd2, rd3, rd4, rd5, rd6, rd7, sync);
+end package read_fsm_pkg;
+
+--package body read_fsm_pkg is
+--
+--end package body read_fsm_pkg;
